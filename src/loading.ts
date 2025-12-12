a41b8f4fd4d229f85d0a3e2e073127c414e2168e
Ying Zhong <0x00eeee@gmail.com>
2025-12-12 13:41:17 +0800

    Improve loading indicator

--- src/loading.ts
+++ src/loading.ts
@@ -1,17 +1,27 @@
+const selector = '.cm-selectionBackground';
+const states: { isLoading: boolean } = { isLoading: false };
+
 const styleSheet = document.createElement('style');
+styleSheet.textContent = `${selector} { transition: opacity 0.4s; }`;
 document.head.append(styleSheet);
 
-styleSheet.textContent = '* { cursor: progress !important }';
-styleSheet.disabled = true;
-
 export function startLoading() {
-  styleSheet.disabled = false;
+  states.isLoading = true;
+  updateClassList();
 }
 
 export function stopLoading() {
-  styleSheet.disabled = true;
+  states.isLoading = false;
+  updateClassList();
 }
 
 export function isLoading() {
-  return !styleSheet.disabled;
+  return states.isLoading;
+}
+
+function updateClassList() {
+  const layers: HTMLElement[] = Array.from(document.querySelectorAll(selector));
+  for (const layer of layers) {
+    layer.style.opacity = states.isLoading ? '0.4' : '1.0';
+  }
 }
